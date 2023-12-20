ceb290e3c98db648b865ea1a0109813addb54de8
motii <108280115+motii8128@users.noreply.github.com>
2023-12-20 11:18:03 +0900

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+extern crate hidapi;
+extern crate dualshock4;
+
+use hidapi::HidApi;
+
+fn main() {
+    let api = HidApi::new().expect("Failed to create HID API instance.");
+    let controller = dualshock4::get_device(&api).expect("Failed to open device");
+
+    loop {
+        let data = dualshock4::read(&controller)
+            .expect("Failed to read data");
+
+        println!("{:?}", data);
+    }
+}
