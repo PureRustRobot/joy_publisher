e8719d612b2e69dba4cda5227690d0575c434093
motii <108280115+motii8128@users.noreply.github.com>
2023-12-20 14:46:29 +0900

    Add files via upload

--- src/bin/axis_publisher.rs
+++ src/bin/axis_publisher.rs
@@ -2,14 +2,37 @@ extern crate hidapi;
 extern crate dualshock4;
 
 use hidapi::HidApi;
+use serde_json;
+
+use zenoh::{
+    config::Config,
+    prelude::r#async::*,
+};
+
+use joy_publisher::get_axis;
 
 fn main() {
     let api = HidApi::new().expect("Failed to create HID API instance.");
     let controller = dualshock4::get_device(&api).expect("Failed to open device");
 
+    let session = zenoh::open(Config::default()).res().await.unwrap();
+    let topic = "/joy".to_string();
+    let publisher = session.declare_publisher(&topic).res().await.unwrap();
+
     loop {
-        let data = dualshock4::read(&controller)
-            .expect("Failed to read data");
+        match dualshock4::read(&controller) {
+            Ok(data)=>
+            {
+                let axis = get_axis(data);
+                let buf = serde_json::to_string(&data).unwrap();
+
+                publisher.put(buf).res().await.unwrap();
+            }
+            Err(e)=>
+            {
+                println!("[ERROR]:{}", e);
+            }
+        }
 
         println!("{:?}",data);
     }
---
+++ src/bin/button_publisher.rs
@@ -0,0 +1,39 @@
+extern crate hidapi;
+extern crate dualshock4;
+
+use hidapi::HidApi;
+use serde_json;
+
+use zenoh::{
+    config::Config,
+    prelude::r#async::*,
+};
+
+use joy_publisher::get_button;
+
+fn main() {
+    let api = HidApi::new().expect("Failed to create HID API instance.");
+    let controller = dualshock4::get_device(&api).expect("Failed to open device");
+
+    let session = zenoh::open(Config::default()).res().await.unwrap();
+    let topic = "/joy".to_string();
+    let publisher = session.declare_publisher(&topic).res().await.unwrap();
+
+    loop {
+        match dualshock4::read(&controller) {
+            Ok(data)=>
+            {
+                let button = get_button(data);
+                let buf = serde_json::to_string(&data).unwrap();
+
+                publisher.put(buf).res().await.unwrap();
+            }
+            Err(e)=>
+            {
+                println!("[ERROR]:{}", e);
+            }
+        }
+
+        println!("{:?}",data);
+    }
+}
